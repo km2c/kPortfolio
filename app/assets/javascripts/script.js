20af58361dde6294bfed555668ac66516f96287f
Kirk M <kirkmccutcheon@gmail.com>
2012-02-27 10:40:41 -0500

    Update routes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,49 +1,53 @@
 (function () {
+  $(function() {
+    
+    //Navigation
+    var pageID = $(document).find('.page').attr('id');
 
-  //Navigation
-  var pageID = $(document).find('.page').attr('id');
+    $('#nav').find('.' + pageID).addClass('active');
 
-  $('#nav').find('.' + pageID).addClass('active');
+    //Slideshow
+    $('#slideshow').after('<div class="control"></div>').cycle({
+      fx: 'fade',
+      speed: 500,
+      timeout: 8000,
+      pager: '.control',
+    });
 
-  //Slideshow
-  $('#slideshow').after('<div class="control"></div>').cycle({
-    fx: 'fade',
-    speed: 500,
-    timeout: 8000,
-    pager: '.control',
+    // Work Page
+    $("a[rel^='prettyPhoto']").prettyPhoto();
+
+    $(".portfolio img").fadeTo("slow", 1.0);
+    $(".portfolio img").hover(function () {
+      $(this).fadeTo(200, 0.4);
+    }, function () {
+      $(this).fadeTo(200, 1.0);
+    });
+
+
+    var showcase = $('.showcase');
+    showcase.hide();
+
+    showcase.first().show();
+
+    $('.showcaseNav a').on('click', function (e) {
+      var url = e.target.href,
+        link = $(this).attr('href');
+
+      if (url.match(/localhost/)) {
+        e.preventDefault();
+        showcase.hide();
+
+        $('.showcaseNav li').removeClass('active');
+        $(this).parent().addClass('active');
+        $('#work').find(link).fadeIn();
+
+      }
+
+    }); 
+    
   });
 
-  // Work Page
-  $("a[rel^='prettyPhoto']").prettyPhoto();
-
-  $(".portfolio img").fadeTo("slow", 1.0);
-  $(".portfolio img").hover(function () {
-    $(this).fadeTo(200, 0.4);
-  }, function () {
-    $(this).fadeTo(200, 1.0);
-  });
-
-
-  var showcase = $('.showcase');
-  showcase.hide();
-
-  showcase.first().show();
-
-  $('.showcaseNav a').on('click', function (e) {
-    var url = e.target.href,
-      link = $(this).attr('href');
-
-    if (url.match(/localhost/)) {
-      e.preventDefault();
-      showcase.hide();
-
-      $('.showcaseNav li').removeClass('active');
-      $(this).parent().addClass('active');
-      $('#work').find(link).fadeIn();
-
-    }
-
-  });
 
 
 })();
