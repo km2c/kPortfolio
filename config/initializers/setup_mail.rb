d4b2be38b83d5966812c1fd2766e5e72e26358ed
km2c <kirkmccutcheon@gmail.com>
2012-04-01 12:15:08 -0300

    Update config/initializers/setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "km2c.com",
-  :user_name            => "kirk@km2c.com",
-  :password             => "mu5t4ng1",
+  :user_name            => "",
+  :password             => "",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
