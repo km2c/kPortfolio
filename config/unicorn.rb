08f3792b44e38ea4fc787a1264b23575621b568a
Kirk M <kirkmccutcheon@gmail.com>
2012-03-31 20:56:46 -0400

    updated config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/kportfolio/current"
+root = "/home/deployer/apps/kPortfolio/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.kportfolio.sock"
+listen "/tmp/unicorn.kPortfolio.sock"
 worker_processes 2
 timeout 30
