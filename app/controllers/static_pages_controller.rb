807b17a9cd0664c9aadd4c6a6cb176752f346dac
Kirk M <kirkmccutcheon@gmail.com>
2012-04-16 09:19:17 -0400

    updated about and resume

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,17 @@
 class StaticPagesController < ApplicationController
   def index
+  	@title = "Home"
   end
 
   def about
+  	@title = "About me"
   end
 
   def resume
+  	@title = "Resume"
   end
 
   def work
+  	@title = "Work Samples"
   end
 end
