22b4e7600ace8ce7123c76aedcd7f8ae388b392e
Kirk M <kirkmccutcheon@gmail.com>
2012-04-16 11:27:50 -0400

    page title logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
+  	@title = "Blog"
   end
 
   def post
   	@post = Post.find(params[:id])
+  	@title = @post.title
   end
 end
